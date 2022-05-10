911b2ff98d70f604aa98178d7b914dc930d39c73
Judo Sky <judoseedsky@gmail.com>
2022-05-10 11:14:31 -0400

    changes

--- bot.js
+++ bot.js
@@ -3,19 +3,31 @@ import client from './opensea.js'
 
 console.log("Bot started");
 
-
 // Tweet
-const tweet = async (_tweet) => {
-    const { data: createdTweet } = await T.v2.tweet(_tweet);
+const tweet = async (aTweet) => {
+    const { data: createdTweet } = await T.v2.tweet(aTweet);
       console.log('Tweet', createdTweet.id, ':', createdTweet.text);
 }
 
-// OpenSea listener
 console.log('listening for sales...');
 
-client.onItemSold("Zipcy's SuperNormal", (event) => {
+// OpenSea listener
+client.onItemSold("supernormalbyzipcy", (event) => {
   // handle event
   console.log(event)
-
-  tweet(event)
+  console.log(event.payload.item.metadata)
+  const { name, metadata_url } = event.payloaditem.metadata;
+  const { eth_price, usd_price } = event.payload.payment_token;
+  const tweetStr = `${name} bought for ${eth_price}Ξ ($${usd_price}) #NFTs ${metadata_url}`;
+  tweet(tweetStr);
 });
+
+// OpenSea listener
+// client.onEvents('supernormalbyzipcy', [EventType.ITEM_RECEIVED_OFFER, EventType.ITEM_TRANSFERRED], (event) => {
+//   // handle event
+//   console.log(event)
+//   const { eth_price, usd_price } = event.payload.payment_token;
+//   const { name, metadata_url } = event.payload;
+//   const tweetStr = `${name} bought for ${eth_price}Ξ ($${usd_price}) #NFTs ${metadata_url}`;
+//   tweet(tweetStr)
+// });
