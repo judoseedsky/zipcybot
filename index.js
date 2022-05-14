63777c124bcebf8c7b888ef96d043863dbd171a5
Judo Sky <judoseedsky@gmail.com>
2022-05-14 11:42:19 -0400

    added comments, fixed missing member acess operator in line 19

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ console.log("Bot started");
 
 // Tweet
 const tweet = async (aTweet) => {
-    const { data: createdTweet } = await T.v2.tweet(aTweet);
-      console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+  const { data: createdTweet } = await T.v2.tweet(aTweet);
+  console.log('Tweet', createdTweet.id, ':', createdTweet.text);
 }
 
 console.log('listening for sales...');
 
 // OpenSea listener
 client.onItemSold("supernormalbyzipcy", (event) => {
-  // handle event
-  console.log(event);
-  console.log(event.payload.item);
-  const { name, metadata_url } = event.payloaditem.metadata;
-  const { eth_price, usd_price } = event.payload.payment_token;
-  const tweetStr = `${name} bought for ${eth_price}Ξ ($${usd_price}) #NFTs ${metadata_url}`;
-  tweet(tweetStr);
+  // Handle event
+  console.log(event); // log event
+  console.log(event.payload.item); // log item sold
+  const { name, metadata_url } = event.payload.item.metadata; // take metadata from item sold (destructer info from it)
+  const { eth_price, usd_price } = event.payload.payment_token; // take price from payment info response
+  const tweetStr = `${name} bought for ${eth_price}Ξ ($${usd_price}) #NFTs ${metadata_url}`; // string together info
+  tweet(tweetStr); // tweet out our string
 });
